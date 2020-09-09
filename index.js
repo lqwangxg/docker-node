6c1dc656c2a255ab06d92be3cc134eb8d2ba9243
lqwangxg <lqwangxg@gmail.com>
2020-09-09 05:40:21 +0000

    add nodemon package for auto restart server.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World, This is updated from outside of container. changed again! ');
 });
 
 app.listen(PORT, HOST);
