37f99ae2a8a6b260032ad7eda943c38c283421a3
lqwangxg <32559839+lqwangxg@users.noreply.github.com>
2020-09-10 11:09:56 +0900

    add botkit

--- index.js
+++ index.js
@@ -2,18 +2,62 @@
 
 // Import Botkit's cor features
 const { Botkit } = reqire('botkit');
+// const { BotkitCMSHelper } = require('botkit-plugin-cms');
 
-const express = require('express');
+const { WebAdapter } = require('botbuilder-adapter-web');
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// Load process.env values from .env file
+require('dotenv').config();
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World, This is updated from outside of container. changed again! ');
+let storage = null;
+if (process.env.MONGO_URI) {
+    storage = mongoStorage = new MongoDbStorage({
+        url : process.env.MONGO_URI,
+    });
+}
+
+const adapter = new WebAdapter({});
+const controller = new Botkit({
+    debug: true,
+    webhook_uri: '/api/messages',
+    adapter: adapter,
+    storage
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + '/features');
+
+  /* catch-all that uses the CMS to trigger dialogs */
+  // if (controller.plugins.cms) {
+  //     controller.on('message,direct_message', async (bot, message) => {
+  //         let results = false;
+  //         results = await controller.plugins.cms.testTrigger(bot, message);
+
+  //         if (results !== false) {
+  //             // do not continue middleware!
+  //             return false;
+  //         }
+  //     });
+  // }
+
+});
+
+
+
+// const express = require('express');
+
+// // Constants
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
+
+// App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello World, This is updated from outside of container. changed again! ');
+// });
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
